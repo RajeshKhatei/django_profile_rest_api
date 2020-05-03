6c766b26db76a9081d2c281b3c621d10a51b08b3
Rajesh Kumar Khatei <rajeshkukhatei@gmail.com>
2020-05-04 00:18:22 +0530

    Added set up to deploy the codes to AWS server

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sqk=cnme+*hy9p__qe=mnp3wt&2m$us-uv$3@xfuhgc=0^qor+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
